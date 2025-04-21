115b43e6872c426442036f214d4fb1b94777fb94
Sunghan <park.libra1011@gmail.com>
2025-04-21 22:15:42 +0900

    feat : 크롤러 구현 * 실행 인자로 행정구역 넣을 경우 실행 될 수 있도록 구현

--- src/main/java/org/prography/Main.java
+++ src/main/java/org/prography/Main.java
@@ -1,39 +1,23 @@
 package org.prography;
 
-import com.google.gson.JsonObject;
-import java.util.List;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import org.prography.geo.GeoRectSlice;
-import org.prography.kakao.KakaoJsonParser;
-import org.prography.kakao.KakaoLocalSearch;
+import org.prography.kakao.client.KakaoLocalApiClient;
+import org.prography.kakao.crawler.KakaoLocalCrawler;
 import org.prography.mongo.MongoDocumentRepository;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
     public static void main(String[] args) {
-        KakaoLocalSearch search = new KakaoLocalSearch();
-        MongoDocumentRepository repo = MongoDocumentRepository.getInstance();
-        GeoRectSlice slice = GeoRectSlice.getInstance();
-        List<String> rectList = slice.sliceRectFromFeature("서울특별시 강남구 삼성1동");
+        Logger mongoLogger = (Logger) LoggerFactory.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.WARN);
 
-        int page = 1;
-        for (String rect : rectList) {
-            JsonObject jsonObject = search.callLocalSearchApi(rect, page);
-            if (!KakaoJsonParser.isEndPage(jsonObject)) {
-                List<JsonObject> documents = KakaoJsonParser.getDocuments(jsonObject);
-                for (JsonObject document : documents) {
-                    String id = KakaoJsonParser.toId(document, "address_name", "place_name");
-                    boolean inserted = repo.saveIfNotExists(id, document);
-                    if (inserted) {
-                        System.out.printf("Inserted: [%s]%n", id);
-                    } else {
-                        System.out.printf("Skip (exists): [%s]%n", id);
-                    }
-
-                }
-                page++;
-            } else {
-                page = 1;
-            }
-        }
+        var client = new KakaoLocalApiClient();
+        var repository = MongoDocumentRepository.getInstance();
+        var rectSlice = GeoRectSlice.getInstance();
+        new KakaoLocalCrawler(client, repository, rectSlice)
+            .crawl(args[0]);
     }
 }
